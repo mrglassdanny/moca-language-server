b1b408af10a665ee63092c9e2e551f4cc8d0e6b7
dglass-longbow <dglass@longbowadvantage.com>
2020-09-01 10:12:31 -0500

    document formatting cleanup/reorg

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/providers/DocumentFormattingProvider.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/providers/DocumentFormattingProvider.java
@@ -4,21 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import com.github.mrglassdanny.mocalanguageserver.MocaLanguageServer;
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.MocaCompiler;
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.antlr.MocaLexer;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.antlr.MocaParser;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.antlr.MocaSqlLexer;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.antlr.MocaSqlParser;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.sql.MocaSqlCompiler;
+
+import com.github.mrglassdanny.mocalanguageserver.moca.lang.format.MocaFormatter;
+
+import com.github.mrglassdanny.mocalanguageserver.moca.lang.sql.format.MocaSqlFormatter;
 import com.github.mrglassdanny.mocalanguageserver.util.lsp.Positions;
 
-import org.antlr.codebuff.misc.LangDescriptor;
 import org.antlr.v4.runtime.Token;
 import org.eclipse.lsp4j.DocumentFormattingParams;
 import org.eclipse.lsp4j.DocumentRangeFormattingParams;
-import org.eclipse.lsp4j.MessageParams;
-import org.eclipse.lsp4j.MessageType;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.TextEdit;
@@ -38,8 +34,8 @@ public class DocumentFormattingProvider {
                         return CompletableFuture.completedFuture(new ArrayList<>());
                 }
 
-                return CompletableFuture.completedFuture(
-                                DocumentOnTypeFormattingProvider.processFormatting(textDocumentContents, mocaCompiler));
+                return CompletableFuture.completedFuture(DocumentFormattingProvider
+                                .processDocumentFormatting(textDocumentContents, mocaCompiler));
 
         }
 
@@ -57,8 +53,76 @@ public class DocumentFormattingProvider {
                         return CompletableFuture.completedFuture(new ArrayList<>());
                 }
 
-                return CompletableFuture.completedFuture(
-                                DocumentOnTypeFormattingProvider.processFormatting(textDocumentContents, mocaCompiler));
+                return CompletableFuture.completedFuture(DocumentFormattingProvider
+                                .processDocumentFormatting(textDocumentContents, mocaCompiler));
+        }
+
+        public static ArrayList<TextEdit> processDocumentFormatting(String textDocumentContents,
+                        MocaCompiler mocaCompiler) {
+                ArrayList<TextEdit> edits = new ArrayList<>();
+
+                String formattedTextDocumentContents = textDocumentContents;
+
+                // Process sql & groovy:
+                for (Token mocaToken : mocaCompiler.mocaTokens) {
+                        if (mocaToken.getType() == MocaLexer.SINGLE_BRACKET_STRING) {
+
+                                String origSqlScript = mocaToken.getText();
+                                // Remove brackets for formatting.
+                                origSqlScript = origSqlScript.substring(1, origSqlScript.length() - 1);
+
+                                try {
+
+                                        String formattedSqlScript = org.antlr.codebuff.Tool.formatForMocaLanguageServer(
+                                                        MocaSqlFormatter.mocaSqlLangDescriptor, origSqlScript,
+                                                        MocaSqlFormatter.mocaSqlCorpus);
+
+                                        // In order for the entire sql script to be indented correctly after formatting,
+                                        // we need to get the char num from the single bracket string token and add that
+                                        // amount spaces/tabs to each newline in formatted sql script.
+                                        StringBuilder indentBuf = new StringBuilder(mocaToken.getCharPositionInLine());
+                                        for (int i = 0; i < mocaToken.getCharPositionInLine(); i++) {
+                                                indentBuf.append(' ');
+                                        }
+                                        formattedSqlScript = formattedSqlScript.replace("\n",
+                                                        "\n" + indentBuf.toString());
+
+                                        // Add to formatted text doc.
+                                        // Dont forget to add brackets back!
+                                        formattedTextDocumentContents = formattedTextDocumentContents
+                                                        .replace(mocaToken.getText(), "[" + formattedSqlScript + "]");
+
+                                } catch (Exception e) {
+                                        // Do nothing...
+                                }
+
+                        } else if (mocaToken.getType() == MocaLexer.DOUBLE_BRACKET_STRING) {
+                                // TODO: format groovy!
+                        }
+                }
+
+                try {
+
+                        String formattedMocaScript = org.antlr.codebuff.Tool.formatForMocaLanguageServer(
+                                        MocaFormatter.mocaLangDescriptor, formattedTextDocumentContents,
+                                        MocaFormatter.mocaCorpus);
+
+                        // If successful, replace formatted text doc with formatted moca script.
+                        formattedTextDocumentContents = formattedMocaScript;
+
+                        // If not successful, formatted text doc will not be changed.
+
+                } catch (Exception e) {
+                        // Formatted text doc remains unchanged.
+                }
+
+                // Add to text doc edits and return!
+                edits.add(new TextEdit(
+                                new Range(new Position(0, 0),
+                                                Positions.getPosition(textDocumentContents,
+                                                                textDocumentContents.length())),
+                                formattedTextDocumentContents));
+                return edits;
         }
 
 }
--- src/main/java/com/github/mrglassdanny/mocalanguageserver/providers/DocumentOnTypeFormattingProvider.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/providers/DocumentOnTypeFormattingProvider.java
@@ -5,15 +5,8 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.MocaCompiler;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.antlr.MocaLexer;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.format.MocaFormatter;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.sql.format.MocaSqlFormatter;
-import com.github.mrglassdanny.mocalanguageserver.util.lsp.Positions;
 
-import org.antlr.v4.runtime.Token;
 import org.eclipse.lsp4j.DocumentOnTypeFormattingParams;
-import org.eclipse.lsp4j.Position;
-import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.TextEdit;
 
 public class DocumentOnTypeFormattingProvider {
@@ -35,71 +28,9 @@ public class DocumentOnTypeFormattingProvider {
                 if (params.getCh().contains("\n")) {
                         return CompletableFuture.completedFuture(new ArrayList<>());
                 } else {
-                        return CompletableFuture.completedFuture(processFormatting(textDocumentContents, mocaCompiler));
+                        return CompletableFuture.completedFuture(DocumentFormattingProvider
+                                        .processDocumentFormatting(textDocumentContents, mocaCompiler));
                 }
-        }
-
-        public static ArrayList<TextEdit> processFormatting(String src, MocaCompiler mocaCompiler) {
-                ArrayList<TextEdit> edits = new ArrayList<>();
-
-                String nxtSrc = src;
-
-                // Process sql & groovy:
-                for (Token mocaToken : mocaCompiler.mocaTokens) {
-                        if (mocaToken.getType() == MocaLexer.SINGLE_BRACKET_STRING) {
-
-                                String origSqlScript = mocaToken.getText();
-                                origSqlScript = origSqlScript.substring(1, origSqlScript.length() - 1);
-
-                                String sqlText = null;
-                                try {
-
-                                        sqlText = org.antlr.codebuff.Tool.formatForMocaLanguageServer(
-                                                        MocaSqlFormatter.mocaSqlLangDescriptor, origSqlScript,
-                                                        MocaSqlFormatter.mocaSqlCorpus);
-
-                                } catch (Exception e) {
-                                        // Do nothing...
-                                }
-
-                                if (sqlText != null) {
-                                        // In order for the entire sql script to be indented correctly after formatting,
-                                        // we need to get the char num from the single bracket string token and add that
-                                        // amount spaces/tabs to each newline in formatted sql script.
-                                        String indention = "";
-                                        for (int i = 0; i < mocaToken.getCharPositionInLine(); i++) {
-                                                indention += " ";
-                                        }
-
-                                        sqlText = sqlText.replace("\n", "\n" + indention);
-
-                                        nxtSrc = nxtSrc.replace(mocaToken.getText(), "[" + sqlText + "]");
-                                }
-
-                        } else if (mocaToken.getType() == MocaLexer.DOUBLE_BRACKET_STRING) {
-
-                        }
-                }
-
-                String finalText = null;
-
-                try {
-
-                        finalText = org.antlr.codebuff.Tool.formatForMocaLanguageServer(
-                                        MocaFormatter.mocaLangDescriptor, nxtSrc, MocaFormatter.mocaCorpus);
-
-                } catch (Exception e) {
-                        // Do nothing...
-                }
-
-                if (finalText != null) {
-                        edits.add(new TextEdit(new Range(new Position(0, 0), Positions.getPosition(src, src.length())),
-                                        finalText));
-                } else {
-                        edits.add(new TextEdit(new Range(new Position(0, 0), Positions.getPosition(src, src.length())),
-                                        nxtSrc));
-                }
-                return edits;
         }
 
 }
