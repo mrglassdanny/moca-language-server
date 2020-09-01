ed777e76457c0e174471b54428e2ac1cda6ae68c
dglass-longbow <dglass@longbowadvantage.com>
2020-09-01 10:16:40 -0500

    made sure is sql string before formatting

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/providers/DocumentFormattingProvider.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/providers/DocumentFormattingProvider.java
@@ -10,6 +10,7 @@ import com.github.mrglassdanny.mocalanguageserver.moca.lang.antlr.MocaLexer;
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.format.MocaFormatter;
 
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.sql.format.MocaSqlFormatter;
+import com.github.mrglassdanny.mocalanguageserver.moca.lang.sql.util.MocaSqlLanguageUtils;
 import com.github.mrglassdanny.mocalanguageserver.util.lsp.Positions;
 
 import org.antlr.v4.runtime.Token;
@@ -67,33 +68,41 @@ public class DocumentFormattingProvider {
                 for (Token mocaToken : mocaCompiler.mocaTokens) {
                         if (mocaToken.getType() == MocaLexer.SINGLE_BRACKET_STRING) {
 
-                                String origSqlScript = mocaToken.getText();
-                                // Remove brackets for formatting.
-                                origSqlScript = origSqlScript.substring(1, origSqlScript.length() - 1);
+                                // Make sure we are dealing with actual sql string.
+                                String tokenText = mocaToken.getText();
+                                if (MocaSqlLanguageUtils.isMocaTokenValueSqlScript(tokenText)) {
+                                        // Remove brackets for formatting.
+                                        String sqlScript = tokenText.substring(1, tokenText.length() - 1);
 
-                                try {
+                                        try {
 
-                                        String formattedSqlScript = org.antlr.codebuff.Tool.formatForMocaLanguageServer(
-                                                        MocaSqlFormatter.mocaSqlLangDescriptor, origSqlScript,
-                                                        MocaSqlFormatter.mocaSqlCorpus);
+                                                String formattedSqlScript = org.antlr.codebuff.Tool
+                                                                .formatForMocaLanguageServer(
+                                                                                MocaSqlFormatter.mocaSqlLangDescriptor,
+                                                                                sqlScript,
+                                                                                MocaSqlFormatter.mocaSqlCorpus);
 
-                                        // In order for the entire sql script to be indented correctly after formatting,
-                                        // we need to get the char num from the single bracket string token and add that
-                                        // amount spaces/tabs to each newline in formatted sql script.
-                                        StringBuilder indentBuf = new StringBuilder(mocaToken.getCharPositionInLine());
-                                        for (int i = 0; i < mocaToken.getCharPositionInLine(); i++) {
-                                                indentBuf.append(' ');
+                                                // In order for the entire sql script to be indented correctly after
+                                                // formatting,
+                                                // we need to get the char num from the single bracket string token and
+                                                // add that
+                                                // amount spaces/tabs to each newline in formatted sql script.
+                                                StringBuilder indentBuf = new StringBuilder(
+                                                                mocaToken.getCharPositionInLine());
+                                                for (int i = 0; i < mocaToken.getCharPositionInLine(); i++) {
+                                                        indentBuf.append(' ');
+                                                }
+                                                formattedSqlScript = formattedSqlScript.replace("\n",
+                                                                "\n" + indentBuf.toString());
+
+                                                // Add to formatted text doc.
+                                                // Dont forget to add brackets back!
+                                                formattedTextDocumentContents = formattedTextDocumentContents.replace(
+                                                                mocaToken.getText(), "[" + formattedSqlScript + "]");
+
+                                        } catch (Exception e) {
+                                                // Do nothing...
                                         }
-                                        formattedSqlScript = formattedSqlScript.replace("\n",
-                                                        "\n" + indentBuf.toString());
-
-                                        // Add to formatted text doc.
-                                        // Dont forget to add brackets back!
-                                        formattedTextDocumentContents = formattedTextDocumentContents
-                                                        .replace(mocaToken.getText(), "[" + formattedSqlScript + "]");
-
-                                } catch (Exception e) {
-                                        // Do nothing...
                                 }
 
                         } else if (mocaToken.getType() == MocaLexer.DOUBLE_BRACKET_STRING) {
