46f90e65dcb8cefc8251388a33f2d8a50ecc7915
dglass-longbow <dglass@longbowadvantage.com>
2020-09-04 13:05:29 -0500

    column caching rework

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/cache/schema/Schema.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/cache/schema/Schema.java
@@ -9,10 +9,82 @@ import com.github.mrglassdanny.mocalanguageserver.moca.connection.MocaConnection
 
 public class Schema {
 
-    private static final String TABLES_SCRIPT = "list user tables description";
+    private static final String TABLES_SCRIPT = "list user tables description\n" +
+                                                "|\n" +
+                                                "list table comment\n" +
+                                                " where table_name = @table_name catch(@?)\n" +
+                                                "|\n" +
+                                                "if (@? = 0)\n" +
+                                                "{\n" +
+                                                "    publish data\n" +
+                                                "     where table_name = @table_name\n" +
+                                                "       and description = nvl(@description, @table_name)\n" +
+                                                "       and table_comment = @table_comment\n" +
+                                                "}\n" +
+                                                "else\n" +
+                                                "{\n" +
+                                                "    publish data\n" +
+                                                "     where table_name = @table_name\n" +
+                                                "       and description = nvl(@description, @table_name)\n" +
+                                                "       and table_comment = ''\n" +
+                                                "}";
     private static final String VIEWS_SCRIPT = "list user views";
-    private static final String TABLE_COLUMNS_SCRIPT = "list user tables | list table columns where table_name = @table_name";
-    private static final String VIEW_COLUMNS_SCRIPT = "list user views | list table columns where table_name = @view_name";
+    private static final String COLUMNS_SCRIPT = "if (dbtype = 'ORACLE')\n" +
+                                                    "{\n" +
+                                                    "    [select col.table_name,\n" +
+                                                    "            col.column_id,\n" +
+                                                    "            col.column_name,\n" +
+                                                    "            col.data_type,\n" +
+                                                    "            col.data_length max_length\n" +
+                                                    "       from sys.all_tab_columns col\n" +
+                                                    "      inner\n" +
+                                                    "       join sys.all_tables t\n" +
+                                                    "         on col.owner = t.owner\n" +
+                                                    "        and col.table_name = t.table_name\n" +
+                                                    "      where col.owner not in ('ANONYMOUS', 'CTXSYS', 'DBSNMP', 'EXFSYS', 'LBACSYS', 'MDSYS', 'MGMT_VIEW', 'OLAPSYS', 'OWBSYS', 'ORDPLUGINS', 'ORDSYS', 'OUTLN', 'SI_INFORMTN_SCHEMA', 'SYS', 'SYSMAN', 'SYSTEM', 'TSMSYS', 'WK_TEST', 'WKSYS', 'WKPROXY', 'WMSYS', 'XDB', 'APEX_040000', 'APEX_PUBLIC_USER', 'DIP', 'FLOWS_30000', 'FLOWS_FILES', 'MDDATA', 'ORACLE_OCM', 'XS$NULL', 'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'PUBLIC')\n" +
+                                                    "     union\n" +
+                                                    "     select col.table_name,\n" +
+                                                    "            col.column_id,\n" +
+                                                    "            col.column_name,\n" +
+                                                    "            col.data_type,\n" +
+                                                    "            col.data_length max_length\n" +
+                                                    "       from sys.all_tab_columns col\n" +
+                                                    "      inner\n" +
+                                                    "       join sys.all_views v\n" +
+                                                    "         on col.owner = v.owner\n" +
+                                                    "        and col.table_name = v.view_name\n" +
+                                                    "      where col.owner not in ('ANONYMOUS', 'CTXSYS', 'DBSNMP', 'EXFSYS', 'LBACSYS', 'MDSYS', 'MGMT_VIEW', 'OLAPSYS', 'OWBSYS', 'ORDPLUGINS', 'ORDSYS', 'OUTLN', 'SI_INFORMTN_SCHEMA', 'SYS', 'SYSMAN', 'SYSTEM', 'TSMSYS', 'WK_TEST', 'WKSYS', 'WKPROXY', 'WMSYS', 'XDB', 'APEX_040000', 'APEX_PUBLIC_USER', 'DIP', 'FLOWS_30000', 'FLOWS_FILES', 'MDDATA', 'ORACLE_OCM', 'XS$NULL', 'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'PUBLIC')\n" +
+                                                    "      order by 1,\n" +
+                                                    "            2,\n" +
+                                                    "            3]\n" +
+                                                    "}\n" +
+                                                    "else if (dbtype = 'MSSQL')\n" +
+                                                    "{\n" +
+                                                    "    [select tab.name as table_name,\n" +
+                                                    "            col.column_id,\n" +
+                                                    "            col.name as column_name,\n" +
+                                                    "            t.name as data_type,\n" +
+                                                    "            col.max_length\n" +
+                                                    "       from sys.tables as tab\n" +
+                                                    "      inner\n" +
+                                                    "       join sys.columns as col\n" +
+                                                    "         on tab.object_id = col.object_id\n" +
+                                                    "       left\n" +
+                                                    "       join sys.types as t\n" +
+                                                    "         on col.user_type_id = t.user_type_id\n" +
+                                                    "     union\n" +
+                                                    "     select object_name(c.object_id) as table_name,\n" +
+                                                    "            c.column_id,\n" +
+                                                    "            c.name as column_name,\n" +
+                                                    "            type_name(user_type_id) as data_type,\n" +
+                                                    "            c.max_length\n" +
+                                                    "       from sys.columns c\n" +
+                                                    "       join sys.views v\n" +
+                                                    "         on v.object_id = c.object_id\n" +
+                                                    "      order by 1,\n" +
+                                                    "            2,\n" +
+                                                    "            3]\n" +
+                                                    "}";
 
     public HashMap<String, Table> tables;
     public HashMap<String, Table> views;
@@ -49,7 +121,12 @@ public class Schema {
         if (res != null) {
             for (int rowIdx = 0; rowIdx < res.getRowCount(); rowIdx++) {
                 String tableName = res.getString(rowIdx, "table_name").toLowerCase();
-                this.tables.put(tableName, new Table(tableName, res.getString(rowIdx, "description")));
+                String tableDescription = res.getString(rowIdx, "description");
+                String tableComment = res.getString(rowIdx, "table_comment");
+                if (!tableComment.isEmpty()) {
+                    tableDescription += ": " + tableComment;
+                }
+                this.tables.put(tableName, new Table(tableName, tableDescription));
             }
 
         }
@@ -78,19 +155,18 @@ public class Schema {
 
         this.columns.clear();
 
-        MocaResults tableColRes = conn.executeCommand(Schema.TABLE_COLUMNS_SCRIPT).results;
-        MocaResults viewColRes = conn.executeCommand(Schema.VIEW_COLUMNS_SCRIPT).results;
+        MocaResults colRes = conn.executeCommand(Schema.COLUMNS_SCRIPT).results;
 
         // Due to how our loop works, we need to save off data here so that we do not
         // leave out the first column of every table(after the first table).
         TableColumn firstColumn = null;
 
         // For table columns and view columns, we can assume that the data is sorted by
-        // table_name, column_name.
-        if (tableColRes != null) {
+        // table_name, column_id, column_name.
+        if (colRes != null) {
 
-            for (int rowIdx = 0; rowIdx < tableColRes.getRowCount(); rowIdx++) {
-                String tableName = tableColRes.getString(rowIdx, "table_name");
+            for (int rowIdx = 0; rowIdx < colRes.getRowCount(); rowIdx++) {
+                String tableName = colRes.getString(rowIdx, "table_name");
                 String curRowTableName = tableName; // Gets updated each row.
                 ArrayList<TableColumn> tableCols = new ArrayList<>();
 
@@ -103,26 +179,20 @@ public class Schema {
 
                 while (tableName.compareToIgnoreCase(curRowTableName) == 0) {
 
-                    tableCols.add(new TableColumn(tableName, tableColRes.getString(rowIdx, "column_name"),
-                            tableColRes.getString(rowIdx, "lngdsc"), tableColRes.getString(rowIdx, "comtyp"),
-                            tableColRes.getInt(rowIdx, "length"), tableColRes.getBoolean(rowIdx, "null_flg"),
-                            tableColRes.getBoolean(rowIdx, "pk_flg"), tableColRes.getBoolean(rowIdx, "ident_flg"),
-                            tableColRes.getString(rowIdx, "column_comment")));
+                    tableCols.add(new TableColumn(tableName, colRes.getString(rowIdx, "column_name"),
+                            colRes.getString(rowIdx, "data_type"), colRes.getInt(rowIdx, "max_length")));
 
-                    if (rowIdx >= tableColRes.getRowCount()) {
+                    if (rowIdx >= colRes.getRowCount()) {
                         break;
                     } else {
                         rowIdx++;
                     }
 
-                    curRowTableName = tableColRes.getString(rowIdx, "table_name");
+                    curRowTableName = colRes.getString(rowIdx, "table_name");
                     if (tableName.compareToIgnoreCase(curRowTableName) != 0) {
                         // New table. Need to save off some info so we dont lose the first column!
-                        firstColumn = new TableColumn(tableName, tableColRes.getString(rowIdx, "column_name"),
-                                tableColRes.getString(rowIdx, "lngdsc"), tableColRes.getString(rowIdx, "comtyp"),
-                                tableColRes.getInt(rowIdx, "length"), tableColRes.getBoolean(rowIdx, "null_flg"),
-                                tableColRes.getBoolean(rowIdx, "pk_flg"), tableColRes.getBoolean(rowIdx, "ident_flg"),
-                                tableColRes.getString(rowIdx, "column_comment"));
+                        firstColumn = new TableColumn(tableName, colRes.getString(rowIdx, "column_name"),
+                        colRes.getString(rowIdx, "data_type"), colRes.getInt(rowIdx, "max_length"));
                     }
                 }
 
@@ -131,53 +201,7 @@ public class Schema {
             }
         }
 
-        // Null out first column for start of views.
-        firstColumn = null;
-
-        if (viewColRes != null) {
-
-            for (int rowIdx = 0; rowIdx < viewColRes.getRowCount(); rowIdx++) {
-                String viewName = viewColRes.getString(rowIdx, "table_name");
-                String curRowViewName = viewName; // Gets updated each row.
-                ArrayList<TableColumn> viewCols = new ArrayList<>();
-
-                // Check if we have a first column set from below.
-                if (firstColumn != null) {
-                    // If so, add it to the new array then null it out for the next go round.
-                    viewCols.add(firstColumn);
-                    firstColumn = null;
-                }
-
-                while (viewName.compareToIgnoreCase(curRowViewName) == 0) {
-
-                    viewCols.add(new TableColumn(viewName, viewColRes.getString(rowIdx, "column_name"),
-                            viewColRes.getString(rowIdx, "lngdsc"), viewColRes.getString(rowIdx, "comtyp"),
-                            viewColRes.getInt(rowIdx, "length"), viewColRes.getBoolean(rowIdx, "null_flg"),
-                            viewColRes.getBoolean(rowIdx, "pk_flg"), viewColRes.getBoolean(rowIdx, "ident_flg"),
-                            viewColRes.getString(rowIdx, "column_comment")));
-
-                    if (rowIdx >= viewColRes.getRowCount()) {
-                        break;
-                    } else {
-                        rowIdx++;
-                    }
-
-                    curRowViewName = viewColRes.getString(rowIdx, "table_name");
-                    if (viewName.compareToIgnoreCase(curRowViewName) != 0) {
-                        // New view. Need to save off some info so we dont lose the first column!
-                        firstColumn = new TableColumn(viewName, viewColRes.getString(rowIdx, "column_name"),
-                                viewColRes.getString(rowIdx, "lngdsc"), viewColRes.getString(rowIdx, "comtyp"),
-                                viewColRes.getInt(rowIdx, "length"), viewColRes.getBoolean(rowIdx, "null_flg"),
-                                viewColRes.getBoolean(rowIdx, "pk_flg"), viewColRes.getBoolean(rowIdx, "ident_flg"),
-                                viewColRes.getString(rowIdx, "column_comment"));
-                    }
-                }
-
-                // Now add to map.
-                this.columns.put(viewName, viewCols);
-            }
-        }
-    }
+   }
 
     public ArrayList<TableColumn> getColumnsForTable(String tableName) {
         return this.columns.get(tableName);
--- src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/cache/schema/TableColumn.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/cache/schema/TableColumn.java
@@ -3,69 +3,20 @@ package com.github.mrglassdanny.mocalanguageserver.moca.cache.schema;
 public class TableColumn {
     public String table_name;
     public String column_name;
-    public String lngdsc;
-    public String comtyp;
-    public int length;
-    public boolean null_flg;
-    public boolean pk_flg;
-    public boolean ident_flg;
-    public String column_comment;
+    public String data_type;
+    public int max_length;
     public String documentationStr;
 
-    public TableColumn(String table_name, String column_name, String lngdsc, String comtyp, int length,
-            boolean null_flg, boolean pk_flg, boolean ident_flg, String column_comment) {
+    public TableColumn(String table_name, String column_name, String data_type, int max_length) {
         this.table_name = table_name.toLowerCase(); // We want all database entities lower case.
         this.column_name = column_name.toLowerCase(); // We want all database entities lower case.
-        this.lngdsc = lngdsc;
-        this.comtyp = comtyp;
-        this.length = length;
-        this.null_flg = null_flg;
-        this.pk_flg = pk_flg;
-        this.ident_flg = ident_flg;
-        this.column_comment = column_comment;
+        this.data_type = data_type;
+        this.max_length = max_length;
 
         this.setDocumentationString();
     }
 
     private void setDocumentationString() {
-        this.documentationStr = "";
-        String dataTypeDescription;
-        switch (this.comtyp) {
-            case "S":
-                dataTypeDescription = "String(" + this.length + ")";
-                break;
-            case "D":
-                dataTypeDescription = "Date";
-                break;
-            case "F":
-                dataTypeDescription = "Numeric";
-                break;
-            case "I":
-                if (this.column_name.contains("flg")) {
-                    dataTypeDescription = "Boolean";
-                } else {
-                    dataTypeDescription = "Integer";
-                }
-                break;
-            default:
-                dataTypeDescription = this.comtyp;
-                break;
-        }
-
-        this.documentationStr += dataTypeDescription;
-        this.documentationStr += "\t\t";
-
-        if (this.pk_flg) {
-            this.documentationStr += "PK\t";
-        }
-        if (!this.null_flg) {
-            this.documentationStr += "Not Null\t";
-        }
-        if (this.ident_flg) {
-            this.documentationStr += "Identity";
-        }
-
-        this.documentationStr += "\n";
-        this.documentationStr += this.column_comment;
+        this.documentationStr = String.format("Data Type: %s\tMax Length: %d", this.data_type.toUpperCase(), this.max_length);
     }
 }
