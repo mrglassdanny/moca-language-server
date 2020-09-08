916eea38ce3018684e717972429e8db1e678610d
dglass-longbow <dglass@longbowadvantage.com>
2020-09-08 10:55:20 -0500

    warning fix

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/connection/exceptions/UnsupportedConnectionTypeException.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/connection/exceptions/UnsupportedConnectionTypeException.java
@@ -2,6 +2,8 @@ package com.github.mrglassdanny.mocalanguageserver.moca.connection.exceptions;
 
 public class UnsupportedConnectionTypeException extends Exception {
 
+    private static final long serialVersionUID = 1L;
+
     public UnsupportedConnectionTypeException(String msg) {
         super(msg);
     }
