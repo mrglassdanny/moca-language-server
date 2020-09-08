4b5b90ee8addb4d5cb6d9fe87322f3623bd36da8
dglass-longbow <dglass@longbowadvantage.com>
2020-09-08 11:35:07 -0500

    default formatter tweaks

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/lang/mocasql/format/MocaSqlFormatter.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/lang/mocasql/format/MocaSqlFormatter.java
@@ -44,7 +44,39 @@ public class MocaSqlFormatter {
 
             Files.createDirectories(Paths.get(defaultPath));
 
-            Files.write(Paths.get(defaultPath + "\\file.mocasql"), "select * from locmst".getBytes());
+            String exampleFormattedMocaSqlScript = "SELECT a, \n" + "       b, \n" + "       c, \n" + "       d, \n"
+                    + "       e \n" + "FROM   l \n" + "WHERE  l.a = '' \n" + "       AND l.b = 'asdf' \n"
+                    + "       AND c = 'asdf' \n" + "       AND d = 10\n" + "\t   \n" + "SELECT DISTINCT a, \n"
+                    + "                b, \n" + "                c, \n" + "                d, \n"
+                    + "                e \n" + "FROM   l \n" + "       JOIN z \n" + "         ON l.a = z.a \n"
+                    + "            AND l.b = z.b \n" + "       LEFT JOIN oo \n" + "              ON z.a = o.a \n"
+                    + "       RIGHT JOIN zz \n" + "               ON z.z = zz.z \n" + "WHERE  l.a = '' \n"
+                    + "       AND l.b = 'asdf' \n" + "       AND c = 'asdf' \n" + "       AND d = 10 \n"
+                    + "GROUP  BY a, \n" + "          b, \n" + "          c \n" + "ORDER  BY a, \n" + "          b, \n"
+                    + "          c \n" + "\n" + "INSERT INTO l \n" + "            (a, \n" + "             b, \n"
+                    + "             c, \n" + "             d) \n" + "VALUES      ('asdf', \n" + "             asdf, \n"
+                    + "             'asdf', \n" + "             654) \n" + "\t\t\t \n" + "UPDATE abc \n"
+                    + "SET    a = '', \n" + "       b = 10, \n" + "       c = asdf \n" + "WHERE  a = '' \n"
+                    + "       AND abc.b = 10 \n" + "       AND abc.c = asdf \n" + "\t   \n" + "DELETE FROM abc \n"
+                    + "WHERE  a = 10 \n" + "       AND b = 'asdf' \n" + "       AND abc.d = foo \n"
+                    + "       AND abc.e = bar \n" + "\t   \n" + "SELECT a, \n" + "       b, \n" + "       c, \n"
+                    + "       d, \n" + "       e \n" + "FROM   l \n" + "WHERE  l.a = '' \n"
+                    + "       AND l.b = 'asdf' \n" + "       AND c = 'asdf' \n" + "       AND d = 10\n" + "\t   \n"
+                    + "SELECT DISTINCT a, \n" + "                b, \n" + "                c, \n"
+                    + "                d, \n" + "                e \n" + "FROM   l \n" + "       JOIN z \n"
+                    + "         ON l.a = z.a \n" + "            AND l.b = z.b \n" + "       LEFT JOIN oo \n"
+                    + "              ON z.a = o.a \n" + "       RIGHT JOIN zz \n" + "               ON z.z = zz.z \n"
+                    + "WHERE  l.a = '' \n" + "       AND l.b = @asdlkfj:raw\n" + "       AND @+asdf\n"
+                    + "       AND @* \n" + "GROUP  BY a, \n" + "          b, \n" + "          c \n" + "ORDER  BY a, \n"
+                    + "          b, \n" + "          c \n" + "\n" + "INSERT INTO l \n" + "            (a, \n"
+                    + "             b, \n" + "             c, \n" + "             d) \n" + "VALUES      ('asdf', \n"
+                    + "             asdf, \n" + "             'asdf', \n" + "             654) \n" + "\t\t\t \n"
+                    + "UPDATE abc \n" + "SET    a = '', \n" + "       b = 10, \n" + "       c = asdf \n"
+                    + "WHERE  a = '' \n" + "       AND abc.b = 10 \n" + "       AND abc.c = nvl(asdf, @asdlfkj) \n"
+                    + "\t   \n" + "DELETE FROM abc \n" + "WHERE  a = 10 \n" + "       AND b = 'asdf' \n"
+                    + "       AND abc.d = @@foo \n" + "       AND abc.e = @bar ";
+
+            Files.write(Paths.get(defaultPath + "\\file.mocasql"), exampleFormattedMocaSqlScript.getBytes());
         }
 
     }
