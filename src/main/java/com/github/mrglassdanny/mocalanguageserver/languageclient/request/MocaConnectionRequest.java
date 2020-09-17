2788104d8c927d63f73cddcdb24707a13f982f1a
dglass-longbow <dglass@longbowadvantage.com>
2020-09-17 11:03:08 -0500

    translated request into json object

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/languageclient/request/MocaConnectionRequest.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/languageclient/request/MocaConnectionRequest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class MocaConnectionRequest {
 
@@ -16,18 +17,22 @@ public class MocaConnectionRequest {
     public boolean useExistingRepository;
 
     public MocaConnectionRequest(List<Object> args) throws Exception {
-        JsonArray jsonArr = (JsonArray) args.get(0);
-        this.url = jsonArr.get(1).getAsString();
-        this.userId = jsonArr.get(2).getAsString();
-        this.password = jsonArr.get(3).getAsString();
+
+        JsonObject connJsonObj = (JsonObject) args.get(0);
+        this.url = connJsonObj.get("url").getAsString();
+        this.userId = connJsonObj.get("user").getAsString();
+        this.password = connJsonObj.get("password").getAsString();
 
         this.classpathList = new ArrayList<>();
-        JsonArray classpathJsonArr = jsonArr.get(4).getAsJsonArray();
+        JsonArray classpathJsonArr = connJsonObj.get("groovyclasspath").getAsJsonArray();
         for (JsonElement jsonElem : classpathJsonArr) {
             this.classpathList.add(jsonElem.getAsString());
         }
 
+        // No need to parse out json structure for this -- we know it will always be a
+        // standalone boolean.
         this.useExistingRepository = ((JsonElement) args.get(1)).getAsBoolean();
+
     }
 
 }
