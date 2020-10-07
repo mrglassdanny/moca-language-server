bdf6308abfb264e0689715c396b23d6a82043d6a
dglass-longbow <dglass@longbowadvantage.com>
2020-10-07 14:28:18 -0500

    moved groovy 'preprocessing' to moca compiler

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/lang/groovy/GroovyCompiler.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/lang/groovy/GroovyCompiler.java
@@ -2,14 +2,9 @@ package com.github.mrglassdanny.mocalanguageserver.moca.lang.groovy;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Map;
 
 import com.github.mrglassdanny.mocalanguageserver.MocaLanguageServer;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.MocaCompilationResult;
-import com.github.mrglassdanny.mocalanguageserver.moca.lang.groovy.util.GroovyLanguageUtils;
-import com.github.mrglassdanny.mocalanguageserver.util.lsp.PositionUtils;
 
-import org.antlr.v4.runtime.Token;
 import org.codehaus.groovy.GroovyBugError;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.control.MultipleCompilationErrorsException;
@@ -24,39 +19,10 @@ public class GroovyCompiler {
 
     public static ArrayList<String> classpathList = new ArrayList<>();
 
-    // Passing in moca script so that we can get redirects( >> res)
-    // from moca so we can add groovy script prefix. We will initialize these as
-    // SimpleResults objects, that way we dont get the static
-    // type check warning and we get intellisense!
-    public static GroovyCompilationResult compileScript(final int rangeIdx, String groovyScript, String mocaScript,
-            MocaCompilationResult mocaCompilationResult) {
+    public static GroovyCompilationResult compileScript(final int rangeIdx, String groovyScript) {
 
         GroovyCompilationResult groovyCompilationResult = new GroovyCompilationResult();
         groovyCompilationResult.compilationUnit = createCompilationUnit();
-
-        // Add prefixes to script.
-        // Also add moca redirects, as long as they were 'declared' above current groovy
-        // script range.
-        String mocaRedirects = "";
-        // Need to keep track of what we have added, that way we dont add 2 redirects
-        // with the same name(will cause static type checking issue).
-        ArrayList<String> addedMocaRedirectNames = new ArrayList<>();
-        int groovyScriptOffset = PositionUtils.getOffset(mocaScript,
-                mocaCompilationResult.groovyRanges.get(rangeIdx).getStart());
-        if (mocaCompilationResult != null) {
-            for (Map.Entry<Token, String> entry : mocaCompilationResult.mocaParseTreeListener.redirects.entrySet()) {
-                if (entry.getKey().getStartIndex() <= groovyScriptOffset) {
-                    String curMocaRedirectName = entry.getValue();
-                    if (!addedMocaRedirectNames.contains(curMocaRedirectName)) {
-                        mocaRedirects += ("SimpleResults " + curMocaRedirectName + "; ");
-                        addedMocaRedirectNames.add(curMocaRedirectName);
-                    }
-                }
-            }
-        }
-
-        groovyScript = GroovyLanguageUtils.GROOVY_DEFAULT_IMPORTS + GroovyLanguageUtils.MOCA_GROOVY_SCRIPT_PREFIX
-                + mocaRedirects + groovyScript;
 
         // Add a source unit to compilation unit and then compile.
         groovyCompilationResult.compilationUnit.addSource(new SourceUnit("script" + rangeIdx, groovyScript,
