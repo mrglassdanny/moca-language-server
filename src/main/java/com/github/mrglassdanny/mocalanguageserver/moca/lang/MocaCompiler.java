72c4bf4f0d25da5e15c7514c36f61b8d8ca2fe22
dglass-longbow <dglass@longbowadvantage.com>
2020-12-19 12:27:12 -0600

    finalize moca compiler script changes compilation

--- src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/lang/MocaCompiler.java
+++ src/main/java/com/github/mrglassdanny/mocalanguageserver/moca/lang/MocaCompiler.java
@@ -14,7 +14,6 @@ import com.github.mrglassdanny.mocalanguageserver.moca.lang.antlr.MocaParser;
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.groovy.GroovyCompiler;
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.groovy.util.GroovyLanguageUtils;
 import com.github.mrglassdanny.mocalanguageserver.moca.lang.mocasql.MocaSqlCompiler;
-import com.github.mrglassdanny.mocalanguageserver.services.MocaServices;
 import com.github.mrglassdanny.mocalanguageserver.util.lsp.PositionUtils;
 import com.github.mrglassdanny.mocalanguageserver.util.lsp.RangeUtils;
 
@@ -24,10 +23,7 @@ import org.antlr.v4.runtime.ConsoleErrorListener;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
-import org.eclipse.lsp4j.util.Positions;
-import org.eclipse.lsp4j.util.Ranges;
 
 public class MocaCompiler {
 
@@ -54,8 +50,10 @@ public class MocaCompiler {
         // Set embedded lang ranges, then compile them.
         mocaCompilationResult.setMocaEmbeddedLanguageRanges(mocaScript);
 
+        // Will use thread pool to compile embedded lang ranges.
         Collection<Callable<Boolean>> compileTasks = new ArrayList<Callable<Boolean>>();
 
+        // Now we go through each embedded lang range and add it to compile list.
         for (MocaEmbeddedLanguageRange mocaEmbeddedLanguageRange : mocaCompilationResult.mocaEmbeddedLanguageRanges) {
 
             switch (mocaEmbeddedLanguageRange.mocaLanguageContext.id) {
@@ -78,6 +76,7 @@ public class MocaCompiler {
             }
         }
 
+        // Compile list.
         try {
             threadPool.invokeAll(compileTasks);
         } catch (InterruptedException ex) {
@@ -88,10 +87,10 @@ public class MocaCompiler {
     }
 
     public static MocaCompilationResult compileScriptChanges(final String mocaScript, final String uriStr,
-            ArrayList<Integer> changedLineNums, MocaCompilationResult previousMocaCompilationResult) {
+            ArrayList<Integer> changedLineNumbers, MocaCompilationResult previousMocaCompilationResult) {
 
-        // Need to compile moca regardless of where the change is, since technically
-        // moca is changed regardless of where the change is lang context-wise.
+        // Need to compile moca regardless of where the change(s) is, since technically
+        // moca is changed regardless of where the change(s) is lang context-wise.
         MocaCompilationResult mocaCompilationResult = new MocaCompilationResult(mocaScript, uriStr);
 
         mocaCompilationResult.mocaTokens = new MocaLexer(CharStreams.fromString(mocaScript)).getAllTokens();
@@ -111,29 +110,28 @@ public class MocaCompiler {
 
         // Now that we have updated embedded lang ranges, the plan is to compare changed
         // line numbers to the ranges and only compile those ranges that contain/border
-        // a changed line number.
+        // a changed line number. Changed line number meaning that a change occured at
+        // that line number.
 
         // Keep existing mocasql/groovy compilation results for now, since only a subset
         // may need to be re-compiled and replaced.
         mocaCompilationResult.mocaSqlCompilationResults = previousMocaCompilationResult.mocaSqlCompilationResults;
         mocaCompilationResult.groovyCompilationResults = previousMocaCompilationResult.groovyCompilationResults;
 
-        // Will use thread pool to compile changes.
+        // Will use thread pool to compile embedded lang range changes.
         Collection<Callable<Boolean>> compileTasks = new ArrayList<Callable<Boolean>>();
 
         // This variable is here to make sure we do not revisit already seen changed
         // line numbers.
-        int visitedChangedLineCount = 0;
+        int visitedChangedLineNumberCount = 0;
+
+        // Now go through embedded lang ranges and find changes.
         for (MocaEmbeddedLanguageRange mocaEmbeddedLanguageRange : mocaCompilationResult.mocaEmbeddedLanguageRanges) {
 
-            MocaServices.logInfoToLanguageClient(String.format("Checking Range: %s", mocaEmbeddedLanguageRange.range));
+            for (int i = visitedChangedLineNumberCount; i < changedLineNumbers.size(); i++) {
+                int changedLineNum = changedLineNumbers.get(i);
 
-            for (int i = visitedChangedLineCount; i < changedLineNums.size(); i++) {
-                int changedLineNum = changedLineNums.get(i);
-
-                MocaServices.logInfoToLanguageClient(String.format("Changed Line Num: %d", changedLineNum));
-
-                // Checking if changed line number is inside/part of range.
+                // Checking if changed line number is contained by/borders range.
                 if (mocaEmbeddedLanguageRange.range.getStart().getLine() <= changedLineNum
                         && mocaEmbeddedLanguageRange.range.getEnd().getLine() >= changedLineNum) {
 
@@ -152,9 +150,8 @@ public class MocaCompiler {
                         });
                     }
 
-                    // Now that we have added range for compilation, we can break from changed line
-                    // nums loop to move
-                    // onto the next range.
+                    // Now that we have added range for compilation, we can break from inner loop to
+                    // move onto the next range.
                     break;
 
                 } else {
@@ -162,21 +159,20 @@ public class MocaCompiler {
                     if (changedLineNum < mocaEmbeddedLanguageRange.range.getStart().getLine()) {
                         // If so, we need to move onto the next changed line number and not revisit this
                         // one.
-                        visitedChangedLineCount++;
+                        visitedChangedLineNumberCount++;
                     }
                 }
             }
 
             // Can leave when we have visited all of the changed line numbers.
-            if (visitedChangedLineCount == changedLineNums.size()) {
+            if (visitedChangedLineNumberCount == changedLineNumbers.size()) {
                 break;
             }
         }
 
-        // Now compile ranges.
+        // Compile list.
         try {
             threadPool.invokeAll(compileTasks);
-            MocaServices.logInfoToLanguageClient("COMPILED RANGES: " + compileTasks.size());
         } catch (InterruptedException ex) {
             // Do nothing..
         }
